474dae4d62d8b8c85004fd449de70b43ca5daac9
Kim Ly <kimly65@gmail.com>
2024-11-10 16:10:18 +0100

    Query random word and show it on the page.

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 import { serve } from "bun";
 import { readFileSync } from "fs";
+import { MongoClient } from "mongodb";
+
+// Connect to mongo db
+let client;
+try {
+  const uri =  `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@ord.c8trc.mongodb.net/`;
+  client = new MongoClient(uri);
+} catch (error) {
+  console.error("Failed to connect to MongoDB", error);
+  process.exit(1);
+}
+
+async function getRandomWord(){
+  try {
+    await client.connect();
+    const db = client.db("ord");
+    const collection = db.collection("ord");
+
+    // Retrieve a random document from the collection
+    const randomWord = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
+    console.log("Random word:", randomWord);
+
+
+    // Check if we have a result and return the word data or a default message
+    if (randomWord.length > 0) {
+      const { word, description, group } = randomWord[0];
+      return { word, description, group };
+    } else {
+      return { word: "No words found", meaning: "", type: "" };
+    }
+    } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    return { word: "Error fetching data", meaning: "", type: "" };
+    } finally {
+    await client.close();
+  }
+}
 
 const server = serve({
   port: 3000,
-  fetch(req) {
+  async fetch(req) {
     const url = new URL(req.url);
 
     // Serve the page.html file when accessing the root path
@@ -18,6 +55,15 @@ const server = serve({
       } catch (error) {
         return new Response("File not found", { status: 404 });
       }
+    }
+
+    // Serve JSON data for the /data route
+    if (url.pathname === "/data") {
+      const wordData = await getRandomWord();
+      console.log("Random word data:", wordData);
+      return new Response(JSON.stringify(wordData), {
+        headers: { "Content-Type": "application/json" },
+      });
     }
 
     // Return 404 for other routes
