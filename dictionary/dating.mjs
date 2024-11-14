485bafec05530526ab036483627f98488024d3c6
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-11-14 10:39:03 +0100

    Updated dictionary handling.

--- dictionary/dating.mjs
+++ dictionary/dating.mjs
@@ -13,44 +13,51 @@ async function date() {
   try {
     await client.connect();
     const database = client.db("ord");
-    const collection = database.collection("ordbok");
+    const dictionaries = {
+      bm: database.collection("bm"),
+      nn: database.collection("nn"),
+    };
 
-    // 1. First get count of documents
-    const count = await collection.countDocuments();
+    for (const dictionary of Object.keys(dictionaries)) {
+      // 1. First get count of documents
+      const count = await dictionaries[dictionary].countDocuments({});
 
-    // 2. Generate sequence of dates and shuffle them
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      // 2. Generate sequence of dates and shuffle them
+      function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
       }
-      return array;
-    }
-    function formatDate(date) {
-      const day = date.getDate().toString().padStart(2, "0");
-      const month = (date.getMonth() + 1).toString().padStart(2, "0");
-      const year = date.getFullYear();
-      return `${day}-${month}-${year}`;
-    }
-    const dates = shuffleArray(
-      Array.from({ length: count }, (_, i) => {
-        const date = new Date();
-        date.setDate(date.getDate() + i);
-        return formatDate(date);
-      }),
-    );
+      function formatDate(date) {
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+      }
+      const dates = shuffleArray(
+        Array.from({ length: count }, (_, i) => {
+          const date = new Date();
+          date.setDate(date.getDate() + i);
+          return formatDate(date);
+        }),
+      );
 
-    // 3. Bulk assign the pre-shuffled unique dates
-    const documents = await collection.find({}, { _id: 0, word: 1 }).toArray();
-    const operations = documents.map((doc, i) => ({
-      updateOne: {
-        filter: { word: doc.word },
-        update: { $set: { date: dates[i] } },
-      },
-    }));
+      // 3. Bulk assign the pre-shuffled unique dates
+      const documents = await dictionaries[dictionary].find({}, { _id: 0, word: 1 }).toArray();
+      const operations = documents.map((doc, i) => {
+        return {
+          updateOne: {
+            filter: { word: doc.word },
+            update: { $set: { date: dates[i], dictionary: `${dictionary}` } },
+          },
+        };
+      });
 
-    await collection.bulkWrite(operations);
-    await collection.createIndex({ date: 1 }, { unique: true });
+      await dictionaries[dictionary].bulkWrite(operations);
+      await dictionaries[dictionary].createIndex({ date: 1 });
+    }
   } finally {
     await client.close();
   }
